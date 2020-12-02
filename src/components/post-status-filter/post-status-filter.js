d671af366556e55cd79250c3e0ffb2d99f54ca4b
Fle1ta <grey5boyko@yndex.ru>
2020-12-02 22:54:32 +0300

    added blog app

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import './post-status-filter.css';
+
+export default class PostStatusFilter extends React.Component {
+    constructor(props){
+        super(props);
+        this.buttons = [
+            {name: 'all',label: 'Все'},
+            {name: 'like',label: 'Понравилось'},
+        ]
+    }
+
+    render(){
+
+        const {filter, onFilterSelect} = this.props;
+        const buttons = this.buttons.map(({name, label}) => {
+            
+            const active = filter === name;
+            const clazz = active ? 'btn-info' : 'btn-outline-secondary';
+            return (
+                <button 
+                    key={name}
+                    className= {`btn ${clazz}`}
+                    type='button'
+                    onClick={()=> onFilterSelect(name)}
+                >
+                    {label}
+                </button>
+            )
+        });
+        return (
+            <div className='btn-group'>
+                {buttons}
+            </div>
+        )
+    }
+}
+
+ 
